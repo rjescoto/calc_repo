ff998e9764555cbbfc2542ebe69a333369900233
rjescoto <rjescoto@gmail.com>
2021-11-14 15:04:02 -0500

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,13 +1,12 @@
-# unit test the calculator lib 
+# unit test the calculator lib
 
 import calculator
 
 
 class testCalculator:
 
-    def testr_add(self):
+    def test_add(self):
         assert 4 == calculator.add(1, 3)
 
-    def testr_add(self):
+    def test_add(self):
         assert 2 == calculator.sub(4, 2)
-        
